e677d03a942c9b7b8ea655bb0ad1d7e66fe5fda2
MustafaWasif <mustafa.wasif123@gmail.com>
2020-06-09 09:34:45 -0700

    db

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -11,7 +11,8 @@ mongoose.connect("mongodb+srv://mustafa:wasif@cluster0-aawk0.mongodb.net/test?re
      useFindAndModify: false,
      useCreateIndex: true
     });
-var db = mongoose.connection;
+//var db = mongoose.connection;
+
 //Creating a Schema
 var todoSchema = new mongoose.Schema({
     item: String
